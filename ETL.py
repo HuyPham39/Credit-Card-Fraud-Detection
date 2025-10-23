c5c151eae170e2734951a9cc833cda66f2b804ef
HuyPham39 <ph.huy3905@gmail.com>
2025-10-23 18:18:01 -0500

    Added more hypotheses to work on the project.

--- ETL.py
+++ ETL.py
@@ -5,8 +5,6 @@ df = pd.read_csv("creditcard.csv")
 # Drop excessive column and duplicates
 df.drop_duplicates(inplace=True)
 df.drop(["Time", "Amount"], axis=1,inplace=True)
-# For better logit linearity
-df.drop(["V17", "V21", "V16", "V27", "V14"], axis=1, inplace=True)
 
 # Export polished dataset
 df.to_csv('ETLdata.csv', index=False)
